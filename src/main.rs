51a3353574461a95a8505012686d8dd754c14deb
James Laughlin <james@sproutbeat.com>
2020-05-24 21:45:18 -0500

    done up to 'Future Tests'

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(peroxide::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
+use peroxide::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -31,42 +29,5 @@ fn panic(info: &PanicInfo) -> !{
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop{}
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]){
-    serial_println!("running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-// #[test_case]
-// fn failed_assertion(){
-//     serial_print!("failing assertion... ");
-//     assert_eq!(1,2);
-//     serial_println!("[ok]");
-// }
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode{
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode){
-    use x86_64::instructions::port::Port;
-
-    unsafe{
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+    peroxide::test_panic_handler(info);
 }
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,23 +1,27 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = 'test_main"]
+#![test_runner(peroxide::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use peroxide::{println, serial_print, serial_println};
 
 #[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> !{
+pub extern "C" fn _start() -> ! {
     test_main();
 
-    loop{}
-}
-
-fn test_runner(tests: &[&dyn Fn()]){
-    unimplemented!();
+    loop {}
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    peroxide::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
