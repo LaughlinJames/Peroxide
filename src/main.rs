7756467b890dffd1af87162c8417ba4cef261b78
James Laughlin <james@sproutbeat.com>
2020-06-06 14:50:15 -0500

    chapter 06 complete

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,11 @@ pub extern "C" fn _start() -> ! {
 
     peroxide::init();
 
-    // invoke breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // fn stack_overflow(){
+    //     stack_overflow();
+    // }
+    //
+    // stack_overflow();
 
     #[cfg(test)]
     test_main();
